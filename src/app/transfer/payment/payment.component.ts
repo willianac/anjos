22d5377d378dd56c9db78d4cef0423916da31e14
Willian Almeida <willianac01@gmail.com>
2023-12-04 09:01:20 -0300

    fix: using the correct maverick script now

--- src/app/transfer/payment/payment.component.ts
+++ src/app/transfer/payment/payment.component.ts
@@ -28,26 +28,37 @@ export class PaymentComponent implements AfterViewInit, OnDestroy {
 		script.type = "text/javascript"
 		script.id = "hosted-form-script"
 		script.text = `
-		(function() {
-			const options = 
-				{"data":"eyJkYmFJZCI6IjEwODIwMiIsInRlcm1pbmFsSWQiOiIxNzI2MDEiLCJ0aHJlZWRzIjoiRGlzYWJsZWQiLCJleHRlcm5hbElkIjoiIiwicmV0dXJuVXJsIjoiaHR0cHM6XC9cL3RyYW5zZmVyYW1lcmljYXMubW9uZXl0cmFuc21pdHRlcnN5c3RlbS5jb21cL21hdmVyaWNrXC8iLCJyZXR1cm5VcmxOYXZpZ2F0aW9uIjoic2VsZiIsImxvZ28iOm51bGwsInZpc2libGVOb3RlIjpudWxsLCJyZXF1ZXN0Q29udGFjdEluZm8iOiJZZXMiLCJyZXF1ZXN0QmlsbGluZ0luZm8iOm51bGwsInNlbmRSZWNlaXB0IjpudWxsLCJvcmlnaW4iOiJIb3N0ZWRGb3JtIiwiaGFzaCI6ImI3Y2M3OTAzNjVkNTFkNGM1ZWI2MWU0YThiZjIzNzA1In0%3D",
-				"amount":${Number(this.totalPayment)},"fee":"","feeType":"amount","contactInfo":{"name":"","email":"","phone":""}};
-	
-			const l = function() {
-				new PaymentGateway({
-					target: "payment-form",
-					options: options
-				}); 
-			};
-	
-			if(typeof window.PaymentGateway === "undefined") {
-				window.webroot = "https://dashboard.maverickpayments.com";
-				const e = document.createElement("script"); e.async = true;
-				e.src = window.webroot + "/js/gateway/payment.js?v=" + Date.now();
-				document.getElementsByTagName("head")[0].appendChild(e);
-				e.onload = e.onreadystatechange = function() { l(); }
-			} else { l(); }
-		}());
+			(function() {
+				const options = {
+					"data":"eyJkYmFJZCI6IjEwODIwMiIsInRlcm1pbmFsSWQiOiIxNzI2MDEiLCJ0aHJlZWRzIjoiRGlzYWJsZWQiLCJleHRlcm5hbElkIjoiIiwicmV0dXJuVXJsIjoiaHR0cHM6XC9cL3RyYW5zZmVyYW1lcmljYXMubW9uZXl0cmFuc21pdHRlcnN5c3RlbS5jb21cL21hdmVyaWNrXC8iLCJyZXR1cm5VcmxOYXZpZ2F0aW9uIjoic2VsZiIsImxvZ28iOm51bGwsInZpc2libGVOb3RlIjpudWxsLCJyZXF1ZXN0Q29udGFjdEluZm8iOm51bGwsInJlcXVlc3RCaWxsaW5nSW5mbyI6IlllcyIsInNlbmRSZWNlaXB0IjpudWxsLCJvcmlnaW4iOiJIb3N0ZWRGb3JtIiwiaGFzaCI6ImY1Yjc0NmZlYjVmZmU5YWU5YzE4NjJmM2U5NjE5MjdiIn0%3D",
+					"amount":${this.totalPayment},
+					"fee":"",
+					"feeType":"amount",
+					"billingInfo":{
+						"country":"United States",
+						"address":"",
+						"address2":"",
+						"city":"",
+						"state":"",
+						"zip":""
+					}
+				};
+
+				const l = function() {
+					new PaymentGateway({
+						target: "payment-form",
+						options: options
+					}); 
+				};
+
+				if(typeof window.PaymentGateway === "undefined") {
+					window.webroot = "https://dashboard.maverickpayments.com";
+					const e = document.createElement("script"); e.async = true;
+					e.src = window.webroot + "/js/gateway/payment.js?v=" + Date.now();
+					document.getElementsByTagName("head")[0].appendChild(e);
+					e.onload = e.onreadystatechange = function() { l(); }
+				} else { l(); }
+			}());
 		`
 		this.renderer.appendChild(this._document.body, script);
 	}

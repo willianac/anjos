4a3150f304c8013bec32bc874d3fd589590eb8d0
Willian Almeida <willianac01@gmail.com>
2023-12-13 11:22:51 -0300

    feat: added error toast when attempting to see invoice history with session expired

--- src/app/pages/invoices-history/invoices-history.component.ts
+++ src/app/pages/invoices-history/invoices-history.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Invoice } from "app/shared/invoices.resolver";
+import { Subscription } from "rxjs";
 
 @Component({
 	selector: 'app-invoices-history',
 	templateUrl: 'invoices-history.component.html',
 	styleUrls: ['invoices-history.component.scss']
 })
-export class InvoicesHistoryComponent implements OnInit {
+export class InvoicesHistoryComponent implements OnInit, OnDestroy {
 	invoiceList: Invoice[] = [];
-
+	invoiceSubscription: Subscription
 	constructor(private activatedRoute: ActivatedRoute) {}
 
 	ngOnInit() {
-		this.activatedRoute.data.subscribe({
+		this.invoiceSubscription = this.activatedRoute.data.subscribe({
 			next: (res) => this.invoiceList = res.invoices.INVOICE
 		})
+	}
+
+	ngOnDestroy() {
+		this.invoiceSubscription.unsubscribe()
 	}
 }
--- src/app/shared/invoices.resolver.ts
+++ src/app/shared/invoices.resolver.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, Resolve } from "@angular/router";
+import { TranslateService } from "@ngx-translate/core";
 import { InvoicesService } from "app/services/invoices/invoices.service";
 const setupData = require("../../assets/setup/setup.json")
 import { AppSetup } from 'assets/setup/setup';
+import { ToastrService } from "ngx-toastr";
 import { Observable } from "rxjs";
 
 export type Invoice = {
@@ -21,12 +23,15 @@ export type Invoice = {
 @Injectable()
 export class InvoicesResolver implements Resolve<Invoice> {
 	private url = ""
-	constructor(private invoicesService: InvoicesService) {
+	constructor(private invoicesService: InvoicesService, private toast: ToastrService, private translate: TranslateService) {
 		const setup = setupData as AppSetup
 		this.url = setup.sampleApiCalls
 	}
 
 	resolve(): Observable<any> {
-		return this.invoicesService.getUserInvoices()
+		return this.invoicesService.getUserInvoices().catch((err) => {
+			this.toast.error(this.translate.instant("SESSION_EXPIRED_TEXT"), this.translate.instant("SESSION_EXPIRED_TITLE"))
+			throw new Error("expirada")
+		})
 	}
 }

6c24c6db5b56d9e512157557013e5e9cdb1af140
Willian Almeida <willianac01@gmail.com>
2023-12-18 14:33:31 -0300

    feat: cash payment page now shows available locations for payment

--- src/app/transfer/cash-payment/cash-payment.component.ts
+++ src/app/transfer/cash-payment/cash-payment.component.ts
@@ -1,8 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { SessionService } from "app/services/session/session.service";
 
 @Component({
 	templateUrl: "cash-payment.component.html",
 	selector: "app-cash-payment",
 	styleUrls: ["cash-payment.component.scss"]
 })
-export class CashPaymentComponent {}
+export class CashPaymentComponent implements OnInit {
+	cityList = []
+	constructor(private session: SessionService) {}
+
+	ngOnInit(): void {
+		const cities = this.session.get("payCities");
+		delete cities.$;
+		this.cityList = [...cities]
+	}
+}

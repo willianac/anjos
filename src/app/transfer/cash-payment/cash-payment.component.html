6626132da5458fa45b9fbfabb0c51e93ddc8b882
Willian Almeida <willianac01@gmail.com>
2023-12-18 12:57:57 -0300

    feat: new page: cash payment page

---
+++ src/app/transfer/cash-payment/cash-payment.component.html
@@ -0,0 +1,1 @@
+<h1>CASH PAYMENT</h1>
---
+++ src/app/transfer/cash-payment/cash-payment.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+	templateUrl: "cash-payment.component.html",
+	selector: "app-cash-payment",
+	styleUrls: ["cash-payment.component.scss"]
+})
+export class CashPaymentComponent {}

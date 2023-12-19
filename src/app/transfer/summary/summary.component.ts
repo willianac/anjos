3c9e2c19b50d83adf8018a47170181544dbfb21b
Willian Almeida <willianac01@gmail.com>
2023-12-18 21:40:04 -0300

    feat: summary page now show details of cash payment location

--- src/app/transfer/summary/summary.component.ts
+++ src/app/transfer/summary/summary.component.ts
@@ -6,7 +6,6 @@ import { TranslateService } from '@ngx-translate/core';
 import { TransferService } from '../../services/transfer/transfer.service';
 
 import { ToastrService } from 'ngx-toastr';
-import { ModalDirective } from 'ngx-bootstrap/modal/modal.component';
 
 import { SenderAccountService } from '../../services/sender-account/sender-account.service';
 import { GeolocationService } from 'app/services/geolocation/geolocation.service';
@@ -32,6 +31,9 @@ export class SummaryComponent {
 
   public senderAccountPage = ['admin', 'senderAccount'];
 
+	public payoutOption = "";
+	public payoutLocation = {};
+
   constructor(
     public session: SessionService,
     public router: Router,
@@ -56,14 +58,8 @@ export class SummaryComponent {
     this.linkInfo = this.session.get('linkInfo');
     this.linkInfo.rate = parseFloat(this.linkInfo.rate).toFixed(2)
     this.total = (parseFloat(this.amount.base) + parseFloat(this.linkInfo.ServiceFee)).toFixed(2);
-    // this.senderAccSvc.getCurrent()
-    //   .then((account) => {
-    //     if (!account) {
-    //       this.toastr.warning(this.translate.instant('NO_ACCOUNTS_REGISTERED'));
-    //       return this.router.navigate(['admin', 'accounts']);
-    //     }
-    //     this.senderAccount = account
-    //   });
+		this.payoutOption = this.session.get("payoutOptionSelected")
+		this.payoutLocation = this.session.get("payoutLocation")
   }
 
 	public checkGeolocation() {
@@ -102,73 +98,6 @@ export class SummaryComponent {
 			this.toastr.error(this.translate.instant("GEOLOCATION_DENIED"), this.translate.instant("ERROR"))
 		}
 	}
-
-  // doTransfer() {
-  //   this.isLoading = true;
-  //   this.transfer.doTransfer(
-  //     this.linkInfo.SessionKey,
-  //     this.receiver.ReceiverID,
-  //     this.receiverAccount.AcctId,
-  //     this.amount.base,
-  //     this.amount.send,
-  //     this.purpose.PurposeId,
-  //     this.senderAccount.account,
-  //     this.senderAccount.aba,
-  //     this.translate.currentLang || this.translate.defaultLang,
-	// 		"teste"
-  //   ).subscribe(
-  //     (response: any) => {
-  //       this.isLoading = false;
-  //       const statusCode = Number(response.StatusCode);
-  //       switch (statusCode) {
-  //         case 1:
-	// 					this.session.set("total", this.total)
-	// 					this.session.set("externalID", response.SendMoney)
-	// 					this.router.navigate(['admin', 'transfer', 'payment']);
-  //           //this.finishTransfer(response.SendMoney);
-  //           break;
-  //         case -2:
-  //           this.session.clear();
-  //           this.backToInit('SESSION_EXPIRED_TITLE', 'SESSION_EXPIRED_TEXT', 'login');
-  //           break;
-  //         case -4:
-  //           this.backToInit('DAYLI_SENDER_EXCEEDED_TITLE', 'DAYLI_SENDER_EXCEEDED_TEXT');
-  //           break;
-  //         case -5:
-  //           this.backToInit('DAYLI_RECEIVER_EXCEEDED_TITLE', 'DAYLI_RECEIVER_EXCEEDED_TEXT');
-  //           break;
-  //         case -8:
-  //           this.backToInit('ERROR', response.SendMoney, 'login');
-  //           break;
-  //         case -9:
-  //           this.backToInit('ERROR', 'BANK_LENGTH_ERROR', this.senderAccountPage);
-  //           break;
-  //         case -10:
-  //           this.backToInit('ERROR', 'ABA_LENGTH_ERROR', this.senderAccountPage);
-  //           break;
-  //         default:
-  //           const text = response.SessionResult || response.SendMoney || this.translate.instant('UNKNOWN_RESPONSE');
-  //           this.message = `[${response.StatusCode}] ${text}`;
-  //           break;
-  //       }
-  //     },
-  //     (err) => {
-  //       this.message = err.message;
-  //       this.isLoading = false;
-  //     },
-  //   );
-  // }
-
-  finishTransfer(text) {
-    const title = this.translate.instant('SUCCESS');
-    this.toastr.success(text, title);
-
-    this.session.remove('currentReceiver');
-    this.session.remove('currentReceiverAccount');
-    this.session.remove('currentPurpose');
-    this.session.remove('currentBase');
-    this.session.remove('currentSend');
-  }
 
   backToInit(title: string, text: string, page?: any) {
     this.toastr.error(this.translate.instant(text), this.translate.instant(title));

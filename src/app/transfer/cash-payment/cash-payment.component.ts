53dcfc7415bc9d6b59fc82525f7ec7db780223f3
Willian Almeida <willianac01@gmail.com>
2023-12-22 17:50:28 -0300

    feat: search available cities via search input on cash payment flow

--- src/app/transfer/cash-payment/cash-payment.component.ts
+++ src/app/transfer/cash-payment/cash-payment.component.ts
@@ -9,7 +9,16 @@ import { SessionService } from "app/services/session/session.service";
 })
 export class CashPaymentComponent implements OnInit {
 	cityList = []
+	filteredCityList = []
+
 	constructor(private session: SessionService, private router: Router) {}
+
+	public searchCity(searchTerm: string) {
+		const filtered = this.cityList.filter(city => {
+			return city.City.toLowerCase().includes(searchTerm.toLowerCase())
+		})
+		this.filteredCityList = filtered
+	}
 
 	public select(city: any) {
 		this.session.remove("currentReceiverAccount")
@@ -21,5 +30,6 @@ export class CashPaymentComponent implements OnInit {
 		 const cities = this.session.get("payOptions").CashPayoutLocation;
 		 delete cities.$;
 		 this.cityList = [...cities]
+		 this.filteredCityList = [...cities]
 	}
 }

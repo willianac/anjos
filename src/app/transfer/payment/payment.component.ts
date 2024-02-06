31a942ff979ed6c312fcb2d24523e675f8de90ba
Willian Almeida <willianac01@gmail.com>
2024-02-06 09:50:55 -0300

    feat: added swiss qr bill generator on payment page

--- src/app/transfer/payment/payment.component.ts
+++ src/app/transfer/payment/payment.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { FormBuilder } from "@angular/forms";
-import { SessionService } from "app/services/session/session.service";
+const SwissQRBill = require("swissqrbill");
+import * as fs from "fs";
 
 @Component({
 	selector: "app-payment",
@@ -19,7 +20,46 @@ export class PaymentComponent {
 		number: [""]
 	})
 
-	constructor(private fb: FormBuilder) {
-
+	public data = {
+		amount: 300,
+		creditor: {
+			account: "CH18 3005 7072 9844 1020 4",
+			address: "Ettenbergstrasse",
+			buildingNumber: 44,
+			city: "Wettswil",
+			country: "CH",
+			name: "Anjos Express Imhof",
+			zip: 8907
+		},
+		currency: 'CHF',
+		debtor: {
+			address: "Sonnhaldenstrasse",
+			buildingNumber: 19,
+			city: "Heerbrugg",
+			country: "CH",
+			name: "Alpha Rheintal Bank",
+			zip: 9435
+		},
+		reference: "00 00007 29844 10204 00232 47902"
 	}
+
+	public teste() {
+		const stream = SwissQRBill.BlobStream()
+		const pdf = new SwissQRBill.PDF(this.data, stream)
+
+		pdf.on("finish", () => {
+			const iframe = document.getElementById("iframe") as HTMLIFrameElement;
+			if(iframe){
+				iframe.src = stream.toBlobURL("application/pdf");
+				console.log("criado")
+			}
+			console.log("PDF has been successfully created.");
+		})
+
+		// const svg = new SwissQRBill.SVG(this.data)
+		// const e = document.querySelector(".card-block") as HTMLDivElement
+		// e.innerHTML = svg
+	}
+
+	constructor(private fb: FormBuilder) {}
 }
---
+++ src/app/types/linkInfo.ts
@@ -0,0 +1,14 @@
+export type LinkInfo = {
+	BaseUnit: string
+	BranchNo: string
+	ExchPerc: string
+	ListLandUnit: string
+	ListSendUnit: string
+	MaxAmountDaily: string
+	MaxOrderAmount: number
+	Rate: string
+	SendUnit: string
+	SenderId: string
+	ServiceFee: string
+	SessionKey: string
+}

f7b757508e6b3f8ca894cd50155f1e94d05e17d0
Willian Almeida <willianac01@gmail.com>
2024-02-07 19:02:06 -0300

    feat: payment page now is a sender bank selection page

--- src/app/transfer/payment/payment.component.ts
+++ src/app/transfer/payment/payment.component.ts
@@ -1,62 +1,67 @@
-import { Component } from "@angular/core";
-import { FormBuilder } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { Account } from "app/pages/sender-account-list/account";
 import { QrBillService } from "app/services/qr-bill/qr-bill.service";
+import { SessionService } from "app/services/session/session.service";
 const SwissQRBill = require("swissqrbill");
+
+const setupData = require("../../../assets/setup/setup.json")
+import { AppSetup } from 'assets/setup/setup';
 
 @Component({
 	selector: "app-payment",
 	templateUrl: "./payment.component.html",
 	styleUrls: ["./payment.component.scss"]
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit {
 	public message = ""
-	receiverForm = this.fb.group({
-		name: [""],
-		iban: [""],
-		country: [""],
-		postcode: [""],
-		town: [""],
-		street: [""],
-		number: [""]
-	})
+	public accountList = []
+	public selectedAccountName;
+	public appSetup: AppSetup;
 
-	constructor(private fb: FormBuilder, private router: Router, private qrBillService: QrBillService) {}
+	constructor(private router: Router, private qrBillService: QrBillService, private session: SessionService) {
+		console.log(SwissQRBill.utils.calculateQRReferenceChecksum("00000000000000000000000325"))
+		this.appSetup = setupData
+	}
 
-	public data = {
-		amount: 300,
-		creditor: {
-			account: "CH1830057072984410204",
-			address: "Ettenbergstrasse",
-			buildingNumber: 44,
-			city: "Wettswil",
-			country: "CH",
-			name: "Anjos Express Imhof",
-			zip: 8907
-		},
-		currency: 'CHF',
-		debtor: {
-			address: "Sonnhaldenstrasse",
-			buildingNumber: 19,
-			city: "Heerbrugg",
-			country: "CH",
-			name: "Alpha Rheintal Bank",
-			zip: 9435
-		},
-		reference: "00 00007 29844 10204 00232 47902"
+	public selectAccount(acc: any) {
+		this.selectedAccountName = acc.name
 	}
 
 	public nextPage() {
+		const senderAccount = this.accountList.find(acc => acc.name === this.selectedAccountName) as Account
+		const data = {
+			amount: 300,
+			creditor: {
+				account: this.appSetup.anjosAccount.account,
+				address: this.appSetup.anjosAccount.address,
+				buildingNumber: this.appSetup.anjosAccount.buildingNumber,
+				city: this.appSetup.anjosAccount.city,
+				country: this.appSetup.anjosAccount.country,
+				name: this.appSetup.anjosAccount.name,
+				zip: this.appSetup.anjosAccount.zip
+			},
+			currency: 'CHF',
+			debtor: {
+				address: senderAccount.address,
+				buildingNumber: senderAccount.buildingNumber,
+				city: senderAccount.city,
+				country: "CH",
+				name: senderAccount.name,
+				zip: senderAccount.zipcode
+			},
+			reference: "00 00000 00000 00100 00112 21007"
+		}
+			
 		const stream = SwissQRBill.BlobStream()
 		try {
-			const pdf = new SwissQRBill.PDF(this.data, stream)
+			const pdf = new SwissQRBill.PDF(data, stream)
 			pdf.on("finish", () => {
 				this.qrBillService.setBillSource(stream.toBlobURL("application/pdf"))
 				this.router.navigate(["admin", "transfer", "qr-bill"])
 			})
 		} catch (error) {
-			console.log("deu erro carai")
-			console.log(error)
+			console.error(error)
 			this.handleBillGeneratorErrors(error.message)
 		}
 
@@ -67,7 +72,18 @@ export class PaymentComponent {
 
 	private handleBillGeneratorErrors(err: string) {
 		if(err.startsWith("The provided IBAN number")) {
-			this.message = "IBAN Invalido"
+			return this.message = "Invalid IBAN"
 		}
+
+		if(err.endsWith("2 characters")) {
+			return this.message = "Country must have 2 characters."
+		}
+
+		return this.message = "Unexpected error"
+
+	}
+
+	ngOnInit(): void {
+		this.accountList = JSON.parse(this.session.get("senderAccounts"))
 	}
 }

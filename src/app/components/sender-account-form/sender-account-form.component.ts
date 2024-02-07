f5fb119b07fdadc47773e222fd46c6c7e3ee23b0
Willian Almeida <willianac01@gmail.com>
2024-02-07 18:09:01 -0300

    feat: added form controls + output property that emit an event when new account is created

--- src/app/components/sender-account-form/sender-account-form.component.ts
+++ src/app/components/sender-account-form/sender-account-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
 import { GeographyService } from "app/services/geography/geography.service";
+import { SessionService } from "app/services/session/session.service";
 
 @Component({
 	selector: "senderAccountForm",
@@ -8,12 +10,37 @@ import { GeographyService } from "app/services/geography/geography.service";
 })
 export class SenderAccountForm implements OnInit {
 	citiesList = []
-	
-	constructor(private geographyService: GeographyService) {}
+	senderAccountForm = this.fb.group({
+		name: [""],
+		city: [""],
+		zipcode: [""],
+		address: [""],
+		buildingNumber: [""]
+	})
+
+	@Output() newAccountEvent = new EventEmitter()
+
+	constructor(private geographyService: GeographyService, private fb: FormBuilder, private session: SessionService) {}
+
+	public createAccount() {
+		const newSenderAccount = this.senderAccountForm.getRawValue()
+		this.appendAccountToLocalStorage(newSenderAccount)
+	}
+
+	private appendAccountToLocalStorage(account: any) {
+		if(!this.session.get("senderAccounts")) {
+			this.session.set("senderAccounts", JSON.stringify([]))
+		}
+		const previousAccounts = JSON.parse(this.session.get("senderAccounts")) as any[]
+		previousAccounts.push(account)
+		this.session.set("senderAccounts", JSON.stringify(previousAccounts))
+
+		this.newAccountEvent.emit()
+	}
 
 	ngOnInit(): void {
-		// this.geographyService.getAllCitiesWithinCountry("CH").subscribe({
-		// 	next: (res) => this.citiesList = res.json()
-		// })
+		this.geographyService.getAllCitiesWithinCountry("CH").subscribe({
+			next: (res) => this.citiesList = res.json()
+		})
 	}
 }

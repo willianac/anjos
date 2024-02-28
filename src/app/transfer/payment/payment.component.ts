475049fd537b0d608b9018b9bcb7940d7c445316
Willian Almeida <willianac01@gmail.com>
2024-02-28 15:50:41 -0300

    feat: will effectively create an invoice when payment page is hit

--- src/app/transfer/payment/payment.component.ts
+++ src/app/transfer/payment/payment.component.ts
@@ -1,7 +1,8 @@
-import { Component } from "@angular/core";
-import { QrBillService } from "app/services/qr-bill/qr-bill.service";
+import { AfterViewInit, Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 import { SenderService } from "app/services/sender/sender.service";
 import { SessionService } from "app/services/session/session.service";
+import { TransferService } from "app/services/transfer/transfer.service";
 const SwissQRBill = require("swissqrbill");
 const setupData = require("../../../assets/setup/setup.json")
 import { AppSetup } from 'assets/setup/setup';
@@ -13,11 +14,17 @@ import { Observable } from "rxjs";
 	templateUrl: "./payment.component.html",
 	styleUrls: ["./payment.component.scss"]
 })
-export class PaymentComponent {
+export class PaymentComponent implements AfterViewInit {
 	public appSetup: AppSetup;
 	public isLoading = false;
 
-	constructor(private session: SessionService, private senderService: SenderService, private toast: ToastrService) {
+	constructor(
+		private session: SessionService, 
+		private senderService: SenderService, 
+		private toast: ToastrService, 
+		private transferService: TransferService,
+		private translateService: TranslateService
+		) {
 		this.appSetup = setupData
 	}
 
@@ -25,8 +32,13 @@ export class PaymentComponent {
 		return this.senderService.getSenderInfo(this.session.get("linkInfo").SenderId)
 	}
 
+	private displayToast(toastTitle: string, toastText) {
+		this.toast.error(this.translateService.instant(toastText), this.translateService.instant(toastTitle))
+	}
+
 	public copyIBAN() {
 		window.navigator["clipboard"].writeText(this.appSetup.anjosAccount.account)
+		this.toast.success("Copiado para área de transferência", "Copiado")
 	}
 
 	public generateQRBill() {
@@ -76,5 +88,52 @@ export class PaymentComponent {
 				this.isLoading = false;
 			},
 		})
+	}
+	
+	private createInvoice() {
+		this.transferService.doTransfer(
+			this.session.get("linkInfo").SessionKey,
+			this.session.get("currentReceiver").ReceiverID,
+			this.session.get("currentReceiverAccount").AcctId,
+			this.session.get("currentBase"),
+			this.session.get("currentSend"),
+			this.session.get("currentPurpose").PurposeId,
+			"000000000",
+			"000000000",
+			this.translateService.currentLang || this.translateService.defaultLang
+		).subscribe((response) => {
+			const statusCode = Number(response.StatusCode);
+			switch (statusCode) {
+				case 1:
+					this.toast.success("Invoice: " + response.SendMoney, this.translateService.instant("SUCCESS"))
+					break;
+				case -2:
+					this.session.clear();
+					this.displayToast('SESSION_EXPIRED_TITLE', 'SESSION_EXPIRED_TEXT');
+					break;
+				case -4:
+					this.displayToast('DAYLI_SENDER_EXCEEDED_TITLE', 'DAYLI_SENDER_EXCEEDED_TEXT');
+					break;
+				case -5:
+					this.displayToast('DAYLI_RECEIVER_EXCEEDED_TITLE', 'DAYLI_RECEIVER_EXCEEDED_TEXT');
+					break;
+				case -8:
+					this.displayToast('ERROR', response.SendMoney);
+					break;
+				case -9:
+					this.displayToast('ERROR', 'BANK_LENGTH_ERROR');
+					break;
+				case -10:
+					this.displayToast('ERROR', 'ABA_LENGTH_ERROR');
+					break;
+				default:
+					this.displayToast('ERROR', 'UNKNOWN_RESPONSE')
+					break;
+			}
+		})
+	}
+
+	ngAfterViewInit(): void {
+		this.createInvoice()
 	}
 }
